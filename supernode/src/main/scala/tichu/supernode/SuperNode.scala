c042ca260339e450ccd3f62b756301185195a25b
Joel <joel.krebs@sv.cmu.edu>
2015-04-12 20:36:32 -0700

    changed config for aws node

--- supernode/src/main/scala/tichu/supernode/SuperNode.scala
+++ supernode/src/main/scala/tichu/supernode/SuperNode.scala
@@ -3,12 +3,12 @@ package tichu.supernode
 import scala.collection.mutable
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
-import akka.event.Logging
 import com.typesafe.config.ConfigFactory
 import tichu.common.SuperNodeMessage.Join
 
 object SuperNode extends App {
-  val system = ActorSystem("RemoteSystem", ConfigFactory.load("supernode"))
+  val config = ConfigFactory.load()
+  val system = ActorSystem("RemoteSystem", config)
   val superNode = system.actorOf(Props[SuperNode], name = "SuperNode")
 }
 
